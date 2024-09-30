e541d44b9636cc5febc96ed6966cfc99902fe864
Vaddemani Siddharth <139576040+siddharthcode98@users.noreply.github.com>
2024-09-30 10:02:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SMOKETREE ASSIGNMENT
+
+### STEP-by-STEP PROCEDURE
+
+- Created a database **userAddressDetails.db** with tables **User** and **Address**.Established a **one-to-many-relationship**
+- Created a Express server in the **index.js**
+- **User** table has 2 columns **id** and **name** and **Address** table has 3 columns **id**,**Address** and **user_id**(foreign key,which is the id of the User table)
+
+
+In the **app.http**, We can make a request to the server. and server provides a response. I have used a VS code extenstion **REST-CLIENT** for this.
---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+
+const app = express();
+app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToDB = path.join(__dirname, "userAddressDetails.db");
+
+let db = null;
+
+const startServerAndConnectDB = async () => {
+  try {
+    db = await open({ filename: pathToDB, driver: sqlite3.Database });
+    app.listen(3000, () => {
+      console.log("server is listening at https://localhost:3000/...");
+    });
+  } catch (error) {
+    console.log(`DB Error:${error.message}`);
+  }
+};
+
+startServerAndConnectDB();
+
+//Register
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, address } = req.body;
+    const getUser = `SELECT id from User WHERE name=?;`;
+    const user = await db.get(getUser, [name]);
+    if (user !== undefined) {
+      const updateAddress = `INSERT INTO Address(address,user_id) VALUES(?,?);`;
+      const data = await db.run(updateAddress, [address, user.id]);
+      res.send(data);
+      res.status(200);
+    } else {
+      const insertNameIntoNameTable = `INSERT INTO User(name) VALUES(?);`;
+      await db.run(insertNameIntoNameTable, [name]);
+      const getUser = `SELECT id from User WHERE name=?;`;
+      const user = await db.get(getUser, [name]);
+      const createNewAddress = `INSERT INTO Address(user_id,address) VALUES(?,?);`;
+      const data = await db.run(createNewAddress, [user.id, address]);
+      res.send(data);
+      res.status(200);
+    }
+
+    //console.log(userId);
+  } catch (error) {
+    console.log(error.message);
+    res.send("error occurred");
+  }
+});
